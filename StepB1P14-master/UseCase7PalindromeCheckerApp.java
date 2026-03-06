c7727d7571a17d56796cf69d8b5f4bbaf1f3c2c0
rohanam390 <rohanam390@gmail.com>
2026-03-06 15:53:13 +0530

    UC8

---
+++ StepB1P14-master/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,37 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "madam";
+
+        Deque<Character> deque = new LinkedList<>();
+
+       
+        for (int i = 0; i < word.length(); i++) {
+            deque.addLast(word.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+       
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
---
+++ StepB1P14-master/Welcome.java
@@ -0,0 +1,21 @@
+public class Welcome{
+
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("=================================");
+        System.out.println(" Welcome to Palindrome Checker ");
+        System.out.println("=================================");
+
+        // Application Details
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Version          : 1.0");
+
+        System.out.println("---------------------------------");
+        System.out.println("The application will check whether");
+        System.out.println("a given string is a palindrome.");
+        System.out.println("---------------------------------");
+
+        System.out.println("Program started successfully...");
+    }
+}
